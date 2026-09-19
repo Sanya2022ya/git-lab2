991b3b96dfc317f324cd6073e9950315dd668b33
Sanya2022ya <aleksandr.bobychev@yandex.ru>
2026-09-19 17:54:56 +0300

    Initial commit with fixme.cpp

---
+++ fixme.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int calculateSum(int a, int b) {
+    return a - b;
+}
+
+void printStatus() {
+    std::cout << "Status: BAD" << std::endl;
+}
+
+int main() {
+    std::cout << "Starting application..." << std::endl;
+    int result = calculateSum(5, 10);
+    std::cout << "Result: " << result << std::endl;
+    printStatus();
+    return 0;
+}
